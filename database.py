2337b57fa553848475cb789af022b41a50b4406a
Saiteja Prathipati <psvvnsaitejap2003@gmail.com>
2023-03-05 12:18:22 +0530

    added some changes in database.py

--- database.py
+++ database.py
@@ -19,9 +19,8 @@ def load_jobs_from_db():
 
     jobs=[]
     for row in result.all():
-        jobs.append(dict(row))
+        jobs.append(dict(row._mapping))
     return jobs
-
 def load_job_from_db(id):
   with engine.connect() as conn:
     result=conn.execute(text('select * from jobs where id= :val'),val=id)
